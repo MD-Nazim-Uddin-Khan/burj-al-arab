2cfa85baf3f6c1fce6c247c13d70ac5c19089084
MD-Nazim-Uddin-Khan <mdnazimuddin5500@gmail.com>
2021-05-15 13:13:39 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbRobCwWA8Dd3utL9fY9wQypyjOfndGg0",
+    authDomain: "burj-al-arab-6afb5.firebaseapp.com",
+    projectId: "burj-al-arab-6afb5",
+    storageBucket: "burj-al-arab-6afb5.appspot.com",
+    messagingSenderId: "559280353112",
+    appId: "1:559280353112:web:3f34d5116d9d6a4cfaff2d"
+  };
+  export default firebaseConfig;
